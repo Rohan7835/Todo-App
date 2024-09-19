c5779a707ed8d4b0df324b4df6e3934f71ceb6c5
rohan7835 <rohan.s7835@gmail.com>
2024-09-19 23:11:16 +0530

    utils setup

---
+++ src/app.js
@@ -0,0 +1,17 @@
+import cookieParser from "cookie-parser";
+import express from "express";
+import cors from "cors";
+
+const app = express();
+
+app.use(cors());
+
+app.use(express.json());
+
+app.use(express.urlencoded({ extended: true }));
+
+app.use(express.static("public"));
+
+app.use(cookieParser());
+
+export { app };
