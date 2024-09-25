6d8c57e227d805975772e75ab6bb4ad70a0dc324
rohan7835 <rohan.s7835@gmail.com>
2024-09-25 17:36:14 +0530

    sub

--- src/models/subscribers.model.js
+++ src/models/subscribers.model.js
@@ -3,28 +3,13 @@ import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
 const SubscribersSchema = mongoose.Schema(
   {
-    title: {
-      type: String,
-      required: true,
-    },
-    completed: {
-      type: Boolean,
-      default: false,
-    },
     owner: {
       type: mongoose.Schema.Types.ObjectId,
       ref: "User",
     },
-    participants: [
-      {
-        type: mongoose.Schema.Types.ObjectId,
-        ref: "User",
-      },
-    ],
-    priority: {
-      type: String,
-      default: "low",
-      enum: ["low", "medium", "high", "critical"],
+    subscriber: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
     },
   },
   { timestamps: true }
